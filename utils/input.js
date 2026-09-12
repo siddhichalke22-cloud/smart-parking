978385cf749f2751aa0dc75cc448287ecab684ae
Siddhi Chalke <siddhichalke22@gmail,com>
2026-09-12 22:52:38 +0530

    fourth commit

---
+++ utils/input.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(question) {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+function closeInput() {
+    rl.close();
+}
+
+module.exports = {
+    ask,
+    closeInput
+};
